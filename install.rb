5407bec0fbfd4d802f6c570c289394f73fa0dd60
Guoliang Cao <gcao@vonage.com>
2009-10-07 09:10:44 -0400

    Fix install path

--- install.rb
+++ install.rb
@@ -3,9 +3,9 @@ puts "Copying files..."
 {
   "validanguage.js" => "/public/javascripts/validanguage.js",
   "validanguage_uncompressed.js" => "/public/javascripts/validanguage_uncompressed.js",
-  "ajax-loader.gif" => "/public/javascripts/ajax-loader.gif",
-  "red_error.gif" => "/public/javascripts/red_error.gif",
-  "validanguage.css" => "/public/javascripts/validanguage.css"
+  "ajax-loader.gif" => "/public/images/ajax-loader.gif",
+  "red_error.gif" => "/public/images/red_error.gif",
+  "validanguage.css" => "/public/stylesheets/validanguage.css"
 }.each do |src, dest|
   src = File.dirname(__FILE__) + src
   dest = RAILS_ROOT + dest
