f38fc0e34cac59158079646746662b075e140ba8
Guoliang Cao <gcao@vonage.com>
2009-10-07 09:23:09 -0400

    Fix install path

--- install.rb
+++ install.rb
@@ -7,8 +7,8 @@ puts "Copying files..."
   "red_error.gif" => "/public/images/red_error.gif",
   "validanguage.css" => "/public/stylesheets/validanguage.css"
 }.each do |src, dest|
-  src = File.dirname(__FILE__) + src
-  dest = RAILS_ROOT + dest
+  src = File.dirname(__FILE__) + "/" + src
+  dest = RAILS_ROOT + "/" + dest
   puts "#{src} => #{dest}"
 	FileUtils.cp_r(src, dest)
 end
